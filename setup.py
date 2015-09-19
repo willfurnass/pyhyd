e00dbec250e1e9f585092f1921f21d4c902be8f8
Will Furnass <will@thearete.co.uk>
2015-09-19 17:28:30 +0100

    Make setup.py PEP8 compliant

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'pyhyd', # Y
-    version = '0.1.5', # Y
-    packages = find_packages(),
+    name='pyhyd',
+    version='0.1.5',
+    packages=find_packages(),
 
-    install_requires = [
+    install_requires=[
         "numpy >= 1.5.1",
         "scipy >= 0.11.0",
     ],
 
-    author = 'Will Furnass',
-    author_email = 'will@thearete.co.uk',
-    description='Functions for calculating various hydraulic quantities relating to ' +
-                'drinking water distribution networks',
+    author='Will Furnass',
+    author_email='will@thearete.co.uk',
+    description='Functions for calculating various hydraulic quantities ' +
+                'relating to drinking water distribution networks',
     license='GPL 3.0',
     keywords='hydraulics drinking water distribution systems',
 )
